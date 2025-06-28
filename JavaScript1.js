6c93113f1103f7112d9e71187cfa30e0097922b2
mike Baradaran <mike.baradaran@qa.com>
2025-06-28 12:52:03 +0100

    refactor adding tags

--- JavaScript1.js
+++ JavaScript1.js
@@ -1,8 +1,15 @@
 ﻿
 setupOptions();
 
+function makeTag(tagName, tagText, tagContainer, tagClass) {
+    let newTag = document.createElement(tagName);
+    newTag.innerText = tagText;
+    newTag.classList.add(tagClass);
+    tagContainer.appendChild(newTag);
+}
+
+// set up the select option tags
 function setupOptions() {
-    // set up the select option tags
     let options = document.getElementById("options");
     for (let i = 0; i < radioNames.length; i++) {
         let span = document.createElement("span");
@@ -29,22 +36,20 @@ function setupOptions() {
 
 let data = [];
 function savedClick() {
-    const learnerName = document.getElementById('learner').value;
+    const learnerName = getValue('learner');
     const fname = learnerName.charAt(0).toUpperCase() + learnerName.slice(1);
 
-    let comments = document.getElementById('comments').value;
+    let comments = getValue('comments');
 
     comments = comments.replace(/<name>/g, fname);
-    console.log(comments);
 
     let learner = {
         'name': learnerName,
         'comments': comments
     };
     for (const attr of radioNames) {
-        learner[attr] = getSelectValue(attr);
+        learner[attr] = getValue(attr);
     }
-
     data.push(learner);
     restForm();
 }
@@ -52,16 +57,14 @@ function savedClick() {
 function generateReports() {
     let report = "Generate a report using the criteria below for each student." +
         "Insert te following at the begining of each student tutor report:\n" +
-        "w/c " + document.getElementById("start_date").value + " - "  +
-    document.getElementById("course_title").value + "\n";
+        "w/c " + getValue("start_date") + " - "  +
+        getValue("course_title") + "\n";
     console.log(report + JSON.stringify(data));
 }
 
 function restForm() {
-    const learnerName = document.getElementById('learner');
-    learnerName.value = "";
-    learnerName.focus();
-    document.getElementById('comments').value = "";
+    setValue('learner', '').focus();
+    setValue('comments', '');
 
     const selects = document.querySelectorAll('select');
     selects.forEach(select => {
@@ -69,7 +72,12 @@ function restForm() {
     });
 }
 
-function getSelectValue(optionID) {
+function getValue(optionID) {
     const op = document.getElementById(optionID);
     return op.value;
+}
+function setValue(optionID, txt) {
+    const x = document.getElementById(optionID);
+    x.value = txt;
+    return x;
 }

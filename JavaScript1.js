9eb4ea6cb073c8393c9c80425885a5a919210fe1
mike Baradaran <mike.baradaran@qa.com>
2025-07-29 20:55:28 +0100

    better!

--- JavaScript1.js
+++ JavaScript1.js
@@ -1,9 +1,37 @@
 ﻿
 let data = getCourseData();
 setupOptions();
+showRegisteredStudents();
+
+function showRegisteredStudents() {
+    let theList = document.getElementById("registeredStudents")
+    theList.innerHTML = "";
+    let data = getCourseData();
+    for (let stu of data) {
+        let op = document.createElement("option");
+        op.innerText = stu.name;
+        theList.appendChild(op);
+    }
+}
+
+function isRegistered(newStuName){
+    let data = getCourseData();
+    for (let stu of data) {
+        if(stu.name.toLowerCase() == newStuName.toLowerCase()) return true;
+    }
+    return false;
+}
 
 function savedClick() {
-    const learnerName = getValue('learner');
+    const learnerName = getValue('learner').trim();
+    if(learnerName.length==0){
+        alert("Please enter the student's name")
+        return;
+    }
+    if(isRegistered(learnerName)){
+        alert("Student name already registered!")
+        return;
+    }
     const fname = learnerName.charAt(0).toUpperCase() + learnerName.slice(1);
 
     let comments = getValue('comments').replace(/<name>/g, fname);
@@ -18,6 +46,7 @@ function savedClick() {
     data.push(learner);
     setCourseData();
     restForm();
+    showRegisteredStudents();
 }
 //-------------------------------------------------------
 function restForm() {
@@ -32,6 +61,10 @@ function restForm() {
 // set up the select option tags --------------------------
 function setupOptions() {
     let options = document.getElementById("options");
+    options.innerHTML = "";
+    radioNames = document.getElementById("radioNames").value.split(",");
+    states = document.getElementById("states").value.split(",");
+
     for (let i = 0; i < radioNames.length; i++) {
         makeTag("span", radioNames[i], options, "feedback-area");
         let select = makeTag("select", "", options)
@@ -39,16 +72,15 @@ function setupOptions() {
         for (const state of states) {
             makeTag("option", state, select);
         }
-        if ( (i+1) % 3 === 0)
+        if ((i + 1) % 3 === 0)
             makeTag("hr", "", options);
     }
     setValue("start_date", new Date().toISOString().split('T')[0]);
 }
 //-------------------------------------------------------------------
 function generateReports() {
-    let report = `Generate an end of course detailed but short report using the criteria below for each student.
-Insert the following at the beginning of each student tutor report:
-w/c ${getValue("start_date")} - ${getValue("course_title")} 
+    let report = `Generate an end of course detailed but short report, using the criteria below for each student.
+Insert "w/c ${getValue("start_date")} - ${getValue("course_title")}" at the start of each student report:${JSON.stringify(data)}
 ${JSON.stringify(data)}`;
 
     navigator.clipboard.writeText(report)
@@ -66,7 +98,7 @@ function setValue(optionID, txt) {
     x.value = txt;
     return x;
 }
-function makeTag(tagName, tagText, tagContainer, tagClass="roundEdge") {
+function makeTag(tagName, tagText, tagContainer, tagClass = "roundEdge") {
     let newTag = document.createElement(tagName);
     newTag.innerText = tagText;
     newTag.classList.add(tagClass);
@@ -89,5 +121,6 @@ function setCourseData() {
 function deleteCourseData() {
     localStorage.removeItem('courseData');
     alert('Course data deleted.');
+    showRegisteredStudents();
 }
 
